69ccb3da7b9414e6789ce93dab6798b53d9e65de
Raihan <nycraihan@gmail.com>
2019-09-08 14:51:14 -0400

    first my project

---
+++ TestNGProject/src/test/java/co/bit/ui/test/RegressionTest.java
@@ -0,0 +1,19 @@
+package co.bit.ui.test;
+
+import org.testng.annotations.Test;
+
+public class RegressionTest extends BaseTest {
+	@Test
+	public void test2 () {
+		 System.out.println("test2");
+		
+	}
+	
+	@Test 
+	public void test3() {
+		 System.out.println("test3");
+		
+		
+	}
+
+}
---
+++ TestNGProject/src/test/java/co/bit/ui/test/SmokeTest.java
@@ -0,0 +1,13 @@
+package co.bit.ui.test;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest {
+	
+	@Test
+	public void test1() {
+		 System.out.println("test1");
+		
+	}
+
+}
